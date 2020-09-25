829ee042f61a1b5bccd2069a4957998830e3231a
AceIsHuman <menoman1995@yahoo.com>
2020-09-25 00:06:14 -0500

    move attribution to own component file

---
+++ src/components/Attribution.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Attribution() {
+  return (
+    <div className="attribution">
+      Challenge by{' '}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a
+        href="https://acielochoa.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Aciel Ochoa
+      </a>
+      .
+    </div>
+  );
+}
+
+export default Attribution;
