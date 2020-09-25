3bb5b00530f4ef206018c4c96e9fd9eaa6f82a83
AceIsHuman <menoman1995@yahoo.com>
2020-09-24 19:17:17 -0500

    build structure for nav-bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <header class="nav-bar">
+      <div class="logo-container">
+        <img src={logo} alt="shortly-logo" />
+      </div>
+      <div class="navigation">
+        <div class="links">
+          <a href="#">Features</a>
+          <a href="#">Pricing</a>
+          <a href="#">Resources</a>
+        </div>
+        <div class="login-signup">
+          <button class="login-btn">Login</button>
+          <button class="sign-up-btn">Sign Up</button>
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default NavBar;
