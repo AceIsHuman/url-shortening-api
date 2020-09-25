c433c109dcdf78824826099cf0ef6a624285e7c1
AceIsHuman <menoman1995@yahoo.com>
2020-09-24 23:43:21 -0500

    add Staircase to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import URLForm from './components/URLForm';
+import Staircase from './components/Staircase';
 
 import './App.css';
 
@@ -11,15 +12,8 @@ function App() {
       <NavBar />
       <Banner />
       <URLForm />
-      Advanced Statistics Track how your
-      links are performing across the web with our advanced statistics
-      dashboard. Brand Recognition Boost your brand recognition with each click.
-      Generic links don’t mean a thing. Branded links help instil confidence in
-      your content. Detailed Records Gain insights into who is clicking your
-      links. Knowing when and where people engage with your content helps inform
-      better decisions. Fully Customizable Improve brand awareness and content
-      discoverability through customizable links, supercharging audience
-      engagement. Boost your links today Get Started Features Link Shortening
+      <Staircase />
+      Boost your links today Get Started Features Link Shortening
       Branded Links Analytics Resources Blog Developers Support Company About
       Our Team Careers Contact
       <div className="attribution">
