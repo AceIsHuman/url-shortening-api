53723b187ba966d104d2556c36682dcc8b0bf5fb
AceIsHuman <menoman1995@yahoo.com>
2020-09-24 23:50:47 -0500

    create structure for BottomCTA

---
+++ src/components/BottomCTA.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BottomCTA() {
+  return (
+    <section className="bottom-cta">
+      <h3 className="bottome-cta-title">Boost your links today</h3>
+      <button className="cta-button">Get Started</button>
+    </section>
+  )
+}
+
+export default BottomCTA;
