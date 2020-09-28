30a95af195f9c940c743b75e942669fa851d27b1
AceIsHuman <menoman1995@yahoo.com>
2020-09-27 23:29:56 -0500

    import useLocalStorage to handle links state | add Links component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import URLForm from './components/URLForm';
+import Links from './components/Links';
 import Staircase from './components/Staircase';
 import BottomCTA from './components/BottomCTA';
 import Footer from './components/Footer';
 import Attribution from './components/Attribution';
 
 import './main.css';
+import useLocalStorage from './utils/useLocalStorage';
 
 function App() {
+  const [links, addLink] = useLocalStorage("links");
+
   return (
     <div className="app">
       <NavBar />
       <Banner />
-      <URLForm />
+      <URLForm addLink={addLink} />
+      <Links links={links} />
       <Staircase />
       <BottomCTA />
       <Footer />
