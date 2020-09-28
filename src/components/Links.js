e4e28aff9e021e551cc2b320dd1e7134c7916e1b
AceIsHuman <menoman1995@yahoo.com>
2020-09-28 01:35:35 -0500

    add functionality to copy button

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Links(props) {
   return (
     <div className="links-container">
       {props.links.map(link => {
-        return <Link link={link} />;
+        return <Link link={link} key={link.hashid} />;
       })}
     </div>
   );
 }
 
 function Link(props) {
+  const copyButton = useRef();
+  const link = 'https://rel.ink/' + props.link.hashid;
+
+  const copy = e => {
+    e.preventDefault();
+    navigator.clipboard.writeText(link)
+      .then(() =>{
+        copyButton.current.innerText = 'Copied!';
+        copyButton.current.classList.add('copied-btn');
+      })
+  }
+
   return (
     <div className="link-card">
       <span className="full-url">{props.link.url}</span>
       <div className="shortly-url-container">
-        <span className="shortly-url">{'https://rel.ink/' + props.link.hashid}</span>
-        <button className="copy-btn">Copy</button>
+        <span className="shortly-url">{link}</span>
+        <button className="copy-btn" ref={copyButton} onClick={copy}>Copy</button>
       </div>
     </div>
   );
