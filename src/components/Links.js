068b340b5382f671a75de6e607d0fc5d430d24cf
AceIsHuman <menoman1995@yahoo.com>
2020-09-27 23:22:45 -0500

    add structure for added links

---
+++ src/components/Links.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Links(props) {
+  return (
+    <div className="links-container">
+      {props.links.map(link => {
+        return <Link link={link} />;
+      })}
+    </div>
+  );
+}
+
+function Link(props) {
+  return (
+    <div className="link-card">
+      <span className="full-url">{props.link.url}</span>
+      <div className="shortyly-url-container">
+        <span className="shortly-url">{'https://rel.ink/' + props.link.hashid}</span>
+        <button className="copy-btn">Copy</button>
+      </div>
+    </div>
+  );
+}
+
+export default Links;
