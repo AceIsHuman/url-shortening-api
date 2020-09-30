74de077e1d46a9f286db81325d14e733f34a6491
AceIsHuman <menoman1995@yahoo.com>
2020-09-29 21:09:08 -0500

    fix typo in bottom-cta-title class

--- src/components/BottomCTA.js
+++ src/components/BottomCTA.js
@@ -3,7 +3,7 @@ import React from 'react';
 function BottomCTA() {
   return (
     <section className="bottom-cta">
-      <h3 className="bottome-cta-title">Boost your links today</h3>
+      <h3 className="bottom-cta-title">Boost your links today</h3>
       <button className="cta-button">Get Started</button>
     </section>
   )
