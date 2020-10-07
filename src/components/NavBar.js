b5298adba6f6f06ff12a1ded7ade42622639b646
AceIsHuman <menoman1995@yahoo.com>
2020-10-06 23:41:16 -0500

    add ability to click outside navMenu to close navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
 import logo from '../images/logo.svg';
 
 function NavBar() {
-  const navigation = useRef();
-  const handleMenu = e => {
-    navigation.current.classList.toggle('open');
-  }
+  const navMenu = useRef();
+  const navContainer = useRef();
+
+  const handleMenu = useCallback(() => {
+    navContainer.current.classList.toggle('open');
+    navMenu.current.classList.toggle('open');
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('click', e => {
+      if (e.target === navContainer.current) {
+        handleMenu();
+      }
+    });
+  }, [handleMenu]);
 
   return (
     <header className="nav-bar">
@@ -13,19 +24,21 @@ function NavBar() {
         <img src={logo} alt="shortly-logo" />
       </div>
       <div className="menu-button" onClick={handleMenu}></div>
-      <div className="navigation" ref={navigation}>
-        <div className="links">
-          <a href="#features">Features</a>
-          <a href="#pricing">Pricing</a>
-          <a href="#resources">Resources</a>
-        </div>
-        <div className="login-signup">
-          <button className="login-btn">Login</button>
-          <button className="sign-up-btn">Sign Up</button>
+      <div className="navigation-container" ref={navContainer}>
+        <div className="navigation" ref={navMenu}>
+          <div className="links">
+            <a href="#features">Features</a>
+            <a href="#pricing">Pricing</a>
+            <a href="#resources">Resources</a>
+          </div>
+          <div className="login-signup">
+            <button className="login-btn">Login</button>
+            <button className="sign-up-btn">Sign Up</button>
+          </div>
         </div>
       </div>
     </header>
-  )
-};
+  );
+}
 
 export default NavBar;
